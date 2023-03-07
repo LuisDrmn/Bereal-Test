6bfbe2199b73159daf303f92b3d02feb099aad56
Jean-Louis Darmon <darmon.jeanlouis@gmail.com>
2023-03-07 11:20:38 +0100

    Initial Commit

---
+++ Beton-Test/Beton_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beton_TestApp.swift
+//  Beton-Test
+//
+//  Created by Jean-Louis Darmon on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Beton_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
